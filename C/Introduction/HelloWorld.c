41409675ad64a66499e0d58ae9ab12f55b9f86c9
SENAPATHI MANOHARAN <76835642+manoharsena@users.noreply.github.com>
2021-01-20 14:00:16 +0530

    Update HelloWorld.c

--- C/Introduction/HelloWorld.c
+++ C/Introduction/HelloWorld.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
 int main() 
 {
-	
     char s[100];
     scanf("%[^\n]%*c", &s);
-  	
+    // [^\n] - everything upto \n, but not \n
+    // %c - scan \n
+    // * - discard what is scanned
+	
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     printf("Hello, World!\n%s",s);    
     return 0;
